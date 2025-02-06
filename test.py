ffe70c62219ff61d167022365f87840fe2899455
c4ffein <c4ffein@gmail.com>
2025-02-06 23:58:23 +0100

    test again

--- test.py
+++ test.py
@@ -2,17 +2,25 @@ from unittest import TestCase
 from unittest import main as unittest_main
 
 class PinnedSSLTest(TestCase):
-    pass
-    # TODO : UT to ensure the checks are called for any python version
-    # TODO : UT to ensure those works if we create 2 contexts with 2 different certificatesa
-    # TODO : UT to ensure check is called if already opened socket gets wrapped
-    # TODO : UT to ensure check is called if connecting on new socket
-    # TODO : Ensure called with correct params, so that regular verif, and so getpeercert is enough
+    def test_the_checks_are_called_for_any_python_version(self):
+        raise Exception("TODO")
+
+    def test_those_works_if_we_create_2_contexts_with_2_different_certificates(self):
+        raise Exception("TODO")
+
+    def test_check_is_called_if_already_opened_socket_gets_wrapped(self):
+        raise Exception("TODO")
+
+    def test_check_is_called_if_connecting_on_new_socket(self):
+        raise Exception("TODO")
+
+    def test_called_with_correct_params_so_that_regular_verif_and_so_getpeercert_is_enough(self):
+        raise Exception("TODO")
 
 
 class BankTest(TestCase):
     def test_bad(self):
-        raise("TODO")
+        raise Exception("TODO")
 
 
 if __name__ == "__main__":
